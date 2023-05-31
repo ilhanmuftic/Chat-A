e8004f5751da98043d5ab69a59589a91cd92eada
Ilhan <ilhanmuftic@gmail.com>
2023-05-31 11:10:48 +0200

    Import.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
+const JWT_SECRET = 'your_secret_key_here';
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const app = express();
+const port = process.env.PORT || 80;
+const io = require('socket.io')(3000)
