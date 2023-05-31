96c3984e95c2737fdbe2aee35f499df5d187752a
Ilhan <ilhanmuftic@gmail.com>
2023-05-31 11:13:29 +0200

    Client setup.

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const port = process.env.PORT || 80;
 const io = require('socket.io')(3000)
+
+app.use(express.static(path.join("public", "static")))
+app.set('trust proxy', 1)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser())
