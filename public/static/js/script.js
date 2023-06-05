4c9c4a0a96967c09b1fca59c65fc93aa02e644ba
Ilhan <ilhanmuftic@gmail.com>
2023-06-05 13:39:14 +0200

    Added Navbar.

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -3,3 +3,11 @@ var stringToHTML = function (str) {
 	var doc = parser.parseFromString(str, 'text/html');
 	return doc.body.getElementsByTagName('*')[0];
 };
+
+function logout(){
+	// Delete the JWT cookie
+	document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+	// Redirect to the login page
+	window.location.href = "/login";
+}
